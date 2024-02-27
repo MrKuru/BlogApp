83a51ec2764ac64d1fea8ccb49630328b57558bd
MrKuru <melihcaan.99@gmail.com>
2024-02-28 00:18:36 +0300

    GetAllPosts endpoint added

--- src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -6,6 +6,9 @@ import com.springboot.blog.repository.PostRepository;
 import com.springboot.blog.service.PostService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class PostServiceImpl implements PostService {
 
@@ -17,19 +20,34 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public PostDto createPost(PostDto postDto) {
-       Post post = new Post();
-        post.setTitle(postDto.getTitle());
-        post.setDescription(postDto.getDescription());
-        post.setContent(postDto.getContent());
+        Post post = mapToEntity(postDto);
+        Post savedPost = postRepository.save(post);
+        PostDto dto = mapToDto(savedPost);
+        return dto;
+    }
 
-       Post savedPost = postRepository.save(post);
+    @Override
+    public List<PostDto> getAllPosts() {
+        List<Post> allPosts = postRepository.findAll();
+        return allPosts.stream()
+                .map(post -> mapToDto(post))
+                .collect(Collectors.toList());
+    }
 
-       PostDto dto = new PostDto();
+    private static PostDto mapToDto(Post savedPost) {
+        PostDto dto = new PostDto();
         dto.setTitle(savedPost.getTitle());
         dto.setContent(savedPost.getContent());
         dto.setDescription(savedPost.getDescription());
         dto.setId(savedPost.getId());
-
         return dto;
+    }
+
+    private static Post mapToEntity(PostDto postDto) {
+        Post post = new Post();
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+        return post;
     }
 }
