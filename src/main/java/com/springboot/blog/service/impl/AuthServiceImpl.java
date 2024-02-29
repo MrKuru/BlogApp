76ebc92616af224dfb0e11f0bfd6810b5424996e
MrKuru <melihcaan.99@gmail.com>
2024-02-29 19:22:38 +0300

    LoginAuth Added.

---
+++ src/main/java/com/springboot/blog/service/impl/AuthServiceImpl.java
@@ -0,0 +1,28 @@
+package com.springboot.blog.service.impl;
+
+import com.springboot.blog.model.LoginDto;
+import com.springboot.blog.service.AuthService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+
+    public AuthServiceImpl(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public String login(LoginDto loginDto) {
+        Authentication authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken
+                (loginDto.getUsernameOrEmail(), loginDto.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authenticate);
+        return "User Logged-in successfully!.";
+    }
+}
